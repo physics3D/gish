a9e428d97bad97788d39b1679c46a88fc74922d4
physics3D <kroener.tobias@t-online.de>
2021-04-03 18:44:08 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,126 @@
+//macro that returns the path to the repository
+macro_rules! repo {
+    () => {
+        &args().nth(1).unwrap();
+    };
+}
+
+use std::{env::args, thread};
+
+use gio::{prelude::ApplicationExtManual, ApplicationExt};
+use glib::PRIORITY_DEFAULT;
+use gtk::{ContainerExt, Grid, GridExt, GtkWindowExt, LabelExt, WidgetExt};
+
+mod myterminal;
+use myterminal::MyTerminal;
+
+use hotwatch::Hotwatch;
+
+const PRETTY_GIT_LOG: &str = "git log --reverse --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+
+fn build_ui(application: &gtk::Application) {
+    let repo_path = repo!();
+    let window = gtk::ApplicationWindow::new(application);
+
+    let title = "gish - git shell - ".to_string() + repo_path;
+    // let title = "gish";
+    window.set_title(&title);
+    window.set_position(gtk::WindowPosition::Center);
+    window.set_default_size(800, 500);
+
+    let mut main_terminal = MyTerminal::new();
+    main_terminal.spawn_shell(repo_path);
+    main_terminal.widget.set_property_expand(true);
+
+    let mut git_log_terminal = MyTerminal::new();
+    git_log_terminal.spawn_command(PRETTY_GIT_LOG, repo_path);
+    git_log_terminal.terminal.set_can_focus(false);
+    git_log_terminal.label.set_text("Git Log");
+    git_log_terminal.widget.set_vexpand(false);
+
+    let mut git_status_terminal = MyTerminal::new();
+    git_status_terminal.spawn_command("echo GIT STATUS; git status --short", repo_path);
+    git_status_terminal.label.set_text("Git Status");
+    git_status_terminal.terminal.set_can_focus(false);
+    git_status_terminal.widget.set_hexpand(false);
+
+    let mut git_branch_terminal = MyTerminal::new();
+    git_branch_terminal.spawn_command("echo GIT BRANCH; git branch", repo_path);
+    git_branch_terminal.label.set_text("Git Branch");
+    git_branch_terminal.terminal.set_can_focus(false);
+    git_branch_terminal.widget.set_hexpand(false);
+
+    let grid = Grid::new();
+
+    grid.attach(&git_log_terminal.widget, 0, 0, 3, 1);
+    grid.attach(&git_status_terminal.widget, 0, 1, 1, 1);
+    grid.attach(&main_terminal.widget, 1, 1, 1, 1);
+    grid.attach(&git_branch_terminal.widget, 2, 1, 1, 1);
+
+    //set focus
+    main_terminal.terminal.grab_focus();
+
+    window.add(&grid);
+
+    window.show_all();
+
+    {
+        let (sender, receiver) = glib::MainContext::channel(PRIORITY_DEFAULT);
+        thread::spawn(move || {
+            let mut hotwatch = Hotwatch::new().expect("hotwatch failed to initialize!");
+            hotwatch
+                .watch(repo!(), move |_| {
+                    println!("foo");
+                    let _ = sender.send("");
+                })
+                .expect("failed to watch directory!");
+            loop {}
+        });
+
+        receiver.attach(None, move |_| {
+            git_status_terminal.restart();
+            git_log_terminal.restart();
+            git_branch_terminal.restart();
+
+            glib::Continue(true)
+        });
+    }
+}
+
+fn print_help_text() {
+    println!("gish: a shell for the git command with additional information about the repository");
+    println!();
+    println!("Usage: gish [-h|--help] [PATH]");
+    println!("Options:");
+    println!("\t-h|--help\tPrints this help text");
+    println!("\tPATH\t\tA path to a git repository");
+}
+
+fn main() {
+    // cheat: make gtk believe we don't handle cli args, give them an empty vector as cli args
+    // and get the arguments ourselves
+
+    if args().len() < 2 {
+        //print please path + help text
+        println!("Please supply a path to a git repository");
+        print_help_text();
+    } else {
+        //no path supplied
+        let arg = args().nth(1).unwrap();
+        if arg == "-h" || arg == "--help" {
+            // print help text
+            print_help_text();
+        } else {
+            // run app
+            let application = gtk::Application::new(Some("com.kroener.tobias"), Default::default())
+                .expect("Initialization failed...");
+
+            application.connect_activate(|app| {
+                build_ui(app);
+            });
+
+            let empty_args = vec!["".to_string()];
+            application.run(&empty_args);
+        }
+    }
+}
---
+++ src/myterminal.rs
@@ -0,0 +1,97 @@
+use std::{path::Path, process::exit};
+
+use gio::Cancellable;
+use glib::SpawnFlags;
+use gtk::{
+    Box, BoxBuilder, ContainerExt, Label, LabelBuilder, Orientation, ScrolledWindow,
+    ScrolledWindowBuilder, WidgetExt,
+};
+use vte::{PtyFlags, Terminal, TerminalExt};
+
+#[derive(Clone)]
+pub struct MyTerminal {
+    pub widget: Box,
+    pub label: Label,
+    pub terminal: Terminal,
+    pub scrolled_window: ScrolledWindow,
+    last_command: String,
+    last_dir: String,
+}
+
+impl MyTerminal {
+    pub fn new() -> Self {
+        let widget = BoxBuilder::new().orientation(Orientation::Vertical).build();
+        let label = LabelBuilder::new().label("MyTerminal").build();
+        let terminal = Terminal::new();
+        terminal.set_property_expand(true);
+        let scrolled_window = ScrolledWindowBuilder::new()
+            .min_content_height(100)
+            .min_content_width(120)
+            .build();
+        scrolled_window.add(&terminal);
+
+        widget.add(&label);
+        widget.add(&scrolled_window);
+
+        Self {
+            widget,
+            label,
+            scrolled_window,
+            terminal,
+            last_command: "".to_string(),
+            last_dir: "".to_string(),
+        }
+    }
+
+    fn spawn(&self, command: &str, directory: &str) {
+        let command_path: Vec<&Path>;
+
+        if command.starts_with("/bin/") {
+            //shell
+            command_path = vec![Path::new(command)];
+        } else {
+            //command
+            command_path = vec![Path::new("/bin/sh"), Path::new("-c"), Path::new(command)];
+        }
+
+        self.terminal
+            .spawn_sync(
+                PtyFlags::DEFAULT,
+                Some(directory),
+                &command_path,
+                &[],
+                SpawnFlags::DO_NOT_REAP_CHILD,
+                Some(&mut || {}),
+                Some(&Cancellable::new()),
+            )
+            .unwrap();
+    }
+
+    pub fn spawn_command(&mut self, command: &str, directory: &str) {
+        self.spawn(command, directory);
+        self.last_command = command.to_string();
+        self.last_dir = directory.to_string();
+    }
+
+    pub fn spawn_shell(&mut self, directory: &str) {
+        let shell = env!("SHELL");
+        self.spawn(shell, directory);
+        self.last_command = shell.to_string();
+        self.last_dir = directory.to_string();
+        self.terminal.connect_child_exited(|_, _| exit(0));
+    }
+
+    #[allow(dead_code)]
+    pub fn spawn_shell_in_home_dir(&mut self) {
+        let home = env!("HOME");
+        self.spawn_shell(home);
+        self.terminal.connect_child_exited(|_, _| exit(0));
+    }
+
+    pub fn restart(&mut self) {
+        self.spawn(
+            &("clear; ".to_string() + &self.last_command),
+            &self.last_dir,
+        );
+    }
+}
